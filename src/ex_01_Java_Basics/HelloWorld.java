b0d2b8872408388e4d5a43129e9f38445ecb7145
danakjuhi <juhidanak@gmail.com>
2025-05-04 12:26:47 +0530

    Updated Git settings

--- src/ex_01_Java_Basics/HelloWorld.java
+++ src/ex_01_Java_Basics/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Commiting again");
+        System.out.println("Updated GIT settings");
     }
 }
